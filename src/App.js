8ab9c4f516b5e0fefffdcb10c389c6f9508121bd
Artech Chu <finalclassic@outlook.com>
2018-08-29 17:52:53 +0800

    临时提交

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-
+import Headerr from './common/header';
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-        hello react.
+        <Headerr />
       </div>
     );
   }
---
+++ src/common/header/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {
+    HeaderWrapper
+} from './style';
+class Header extends Component {
+    redner() {
+        return (
+            <HeaderWrapper>header</HeaderWrapper>
+        )
+    }
+}
+
+export default Header;
