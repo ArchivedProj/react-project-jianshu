3df4924090643f598e6694a51bb33ae2d4a2eed8
Artech Chu <finalclassic@outlook.com>
2018-08-29 18:10:35 +0800

    标题栏左右两侧文字完成样式

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import {
     HeaderWrapper,
-    Logo
+    Logo,
+    Nav,
+    NavItem
 } from './style';
 class Header extends Component {
     render() {
         return (
             <HeaderWrapper>
                 <Logo />
+                <Nav>
+                    <NavItem className='left active'>首页</NavItem>
+                    <NavItem className='left'>下载App</NavItem>
+                    <NavItem className='right'>登陆</NavItem>
+                    <NavItem className='right'>Aa</NavItem>
+                </Nav>
             </HeaderWrapper>
         )
     }
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -7,7 +7,7 @@ export const HeaderWrapper = styled.div`
 `
 
 export const Logo = styled.a.attrs({
-    href:'/'
+    href: '/'
 })`
 position:absolute;
 top:0;
@@ -17,4 +17,27 @@ width:100px;
 height:56px;
 background:url(${logoImg});
 background-size:contain;
+`
+
+export const Nav = styled.div`
+    width:960px;
+    height:100%;
+    margin:0 auto;
+`
+
+export const NavItem = styled.div`
+    color:#333;
+    line-height:56px;
+    padding:0 15px;
+    font-size:17px;
+    &.left{   //表示NavItem组件上有一个className=left
+        float:left;
+    }
+    &.right{
+        float:right;
+        color:#969696;
+    }
+    &.active{
+        color:#ea6f5a;
+    }
 `
