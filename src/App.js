3e534eec67f21b036af10cc52d8bbfee81d972e7
Artech Chu <finalclassic@outlook.com>
2018-09-02 18:06:18 +0800

    简单实用 react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import Header from './common/header';
 import store from './store/index';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom'
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Header />
+        <div>
+          <Header />
+          <BrowserRouter>
+            <div>
+              <Route path='/'  render={() => <div>首页</div>}></Route>
+              <Route path='/detail'  render={() => <div>Detail page</div>}></Route>
+            </div>
+          </BrowserRouter>
+        </div>
       </Provider>
     );
   }
