b0a616ed30bd460579e825d39d36d5b755320a16
Artech Chu <finalclassic@outlook.com>
2018-09-06 14:50:39 +0800

    调整引用路径，防止报错，writer-> write

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,7 +4,7 @@ import { reducer as headerReducer } from '../common/header/store';
 import { reducer as homeReducer } from '../pages/home/store';
 import { reducer as detailReducer } from '../pages/detail/store';
 import { reducer as loginReducer } from '../pages/login/store';
-import { reducer as writeReducer } from '../pages/writer/store';
+import { reducer as writeReducer } from '../pages/write/store';
 
 export default combineReducers({
     header: headerReducer,
